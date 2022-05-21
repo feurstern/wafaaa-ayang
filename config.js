c351bba6b0b54349df95ddd9ad66fddc7b81f1f8
Muhammad Rio Kurniawan <60057220+feurstern@users.noreply.github.com>
2022-05-21 21:43:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
 const CONFIG = {
-    titleWeb: "Ayang Wafa",
-    introTitle: 'Haiiii, malam ayang Wafa. Kangennn sama kamuuu ',
-    introDesc: `Tolong kamu coba explore webnya ya. Mungkin aja ada sesuatu yang menarik wkkwwk. Btw, coba turn on volume-nya atau enggak pake earphone ya.`,
-    btnIntro: 'Next',
-    title: 'Kamu punya perasaan enggak sih sama aku?',
-    desc: 'Jawab aja yang jujur gpp kok. Oh iya btw, button "enggak" lagi error. Jadinya cuma bisa pake button satu lagi wkkwkwkw',
-    btnYes: 'Iyaaaa',
-    btnNo: 'Enggakkkkk',
-    question: 'Ihhhh makasih jawabannya ayang Wafaa. Apa ada hal lain yang mau disampaikan?',
-    btnReply: 'Next',
-    reply: '',
-    mess: 'Makasihhh ayang udah bukan web-nya',
-    messDesc: 'Aku lakuin begini emang beneran serius kok sama kamu, Wafa. Nanti aku akan tunjukin hal - hal yang lain buat buktiin kalo aku beneran serius sama kamu. Btw, happy weekend .Love youuuu',
-    btnAccept: 'ok',
-    messLink: './img/cuteCat.jpg' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
